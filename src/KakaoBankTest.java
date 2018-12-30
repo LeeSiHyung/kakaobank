4a941856dc3f7838f07289db287bff6e7fcd39e6
이시형 <dltlgud1324@gmail.com>
2018-12-31 00:01:45 +0900

    test

--- src/KakaoBankTest.java
+++ src/KakaoBankTest.java
@@ -1,4 +1,4 @@
-package com.kakaobank.app;
+
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -16,8 +16,20 @@ public class KakaoBankTest {
 	
 	static final String READ_FILE_NAME = "comments.csv";
 	static final String RESULT_FILE_PATH = "result.txt";
+	static final String[] SCHOOL_FREFIX_SUBTRING_2BYTE_ARRAY = {"여자","체육"};
 	
-	public static void main(String[] args) throws IOException {
+	enum SchoolType{
+		E("초등학교"), M("중학교"), H("고등학교"), U("대학교"), GM("여중"), GH("여고"), WU("여대");
+		final String type;
+		SchoolType(String type){
+			this.type = type;
+		}
+		public String getType() {
+			return type;
+		}
+	}
+	
+	public static void main(String[] args){
 		
 		if(args.length <= 0) {
 			System.out.println("java argument에[comments.cvs]파일이 저장된 디렉토리 경로를 지정해주세요."); 
@@ -26,26 +38,27 @@ public class KakaoBankTest {
 		
 		String rootPath = args[0];
 		
-		// 1. 처리
-		Path readPath = Paths.get(rootPath, READ_FILE_NAME);
-		if(!Files.exists(readPath)) {
-			System.out.println("지정된 경로에 [comments.cvs]파일이  존재하지 않습니다.");
-			return;
-		}
-		
-		byte[] fileBytes = null;
 		try {
-			fileBytes = Files.readAllBytes(readPath);
+			
+			// 1. 처리
+			Path readPath = Paths.get(rootPath, READ_FILE_NAME);
+			if(!Files.exists(readPath)) {
+				System.out.println("지정된 경로에 [comments.cvs]파일이  존재하지 않습니다.");
+				return;
+			}
+			
+			String contents = new String(Files.readAllBytes(readPath), StandardCharsets.UTF_8);
+			
+			Map<String, Long> schoolCount = getSchoolCountMap(contents);
+			
+			// 2. 출력
+			Path writePath = Paths.get(rootPath, RESULT_FILE_PATH);
+			if(Files.exists(writePath)) Files.delete(writePath); Files.createFile(writePath);
+			writeFile(writePath, schoolCount);
+			
 		} catch (IOException e) {
-			System.err.println("Files.readAllBytes Error " + e.getMessage());
+			System.err.println("Files Operation Error " + e.getMessage());
 		}
-		
-		Map<String, Long> schoolCount = getSchoolCountMap(new String(fileBytes, StandardCharsets.UTF_8));
-		
-		// 2. 출력
-		Path writePath = Paths.get(rootPath, RESULT_FILE_PATH);
-		if(Files.exists(writePath)) Files.delete(writePath); Files.createFile(writePath);
-		writeFile(writePath, schoolCount);
 		
 	}
 
@@ -58,8 +71,8 @@ public class KakaoBankTest {
 				.parallelStream()
 				// 1.3 필터
 				.filter(s -> {
-					// 1.3.1 SchoolType 해당하는 단어로 포함되거나 시작되지 않는 항목 검색 (예: 중학교, 고등학교)
 					for(SchoolType type : SchoolType.values()) {
+						// 1.3.1 학교타입을 포함하거나, 학교명이 없는 경우는 제외 (예: OO중학교(O), 중학교(X) )
 						if(s.contains(type.getType()) && !s.startsWith(type.getType())){
 							return true;
 						}
@@ -70,19 +83,21 @@ public class KakaoBankTest {
 				.collect(Collectors.groupingByConcurrent(s -> {
 					for(SchoolType schoolType : SchoolType.values()) {
 						if(s.contains(schoolType.getType())){
-							// 1.4.1 SchoolType 이후 단어는 삭제 (예: 중학교를 -> 중학교)
+							// 1.4.1 학교명 이후단어 삭제 (예: 중학교OOOO -> 중학교)
 							s = s.substring(0, s.indexOf(schoolType.getType()) + schoolType.getType().length());
 							
-							// 1.4.2 학교명칭을 축약
-							if(s.indexOf(schoolType.getType()) >= 6 && s.indexOf("여자상업") > 0) {
-								s = s.substring(s.indexOf(schoolType.getType()) - 6);
+							// 1.4.2 학교명 이전단어 추출 (예 : OO고등학교, OO대학교)
+							boolean flag = false;
+							for(String arr : SCHOOL_FREFIX_SUBTRING_2BYTE_ARRAY) {
+								if(s.indexOf(arr) >= 2) {
+									s = s.substring(s.indexOf(arr) - 2);
+									flag = true;
+								}
 							}
-							else if(s.indexOf(schoolType.getType()) >= 4 && (s.indexOf("여자") > 0 || s.indexOf("체육") > 0 || s.indexOf("상업") > 0 || s.indexOf("농업") > 0)) {
-								s = s.substring(s.indexOf(schoolType.getType()) - 4);
-							}
-							else if(s.indexOf(schoolType.getType()) >= 2){
+							if(!flag && s.indexOf(schoolType.getType()) >= 2){
 								s = s.substring(s.indexOf(schoolType.getType()) - 2);
 							}
+							
 						}
 					}
 					return s;
@@ -91,8 +106,7 @@ public class KakaoBankTest {
 				}, Collectors.counting()));
 	}
 	
-	private static void writeFile(Path writePath, Map<String, Long> wordCount) throws IOException {
-		
+	private static void writeFile(Path writePath, Map<String, Long> wordCount){
 		// 2.1 학교카운트 line별 write
 		wordCount.forEach( (k, v) -> 
 			{
@@ -101,31 +115,10 @@ public class KakaoBankTest {
 					// 2.1.1  라인단위로 학교명 + \t + 카운트 + \n write
 					Files.write(writePath, (k + "\t" + v + "\n").getBytes(), StandardOpenOption.APPEND);
 				} catch (IOException e) {
-					System.err.println("Files.write Error : " + e.getMessage());
+					System.err.println("Files write Error : " + e.getMessage());
 				}
 			}
 		);
-	}
-	
-	enum SchoolType{
-		ELEMENTARY("초등학교"),
-		MIDDLE("중학교"),
-		HIGH("고등학교"),
-		UNIVERSITY("대학교"),
-		GIRLS_MIDDLE("여중"),
-		GIRLS_HIGH("여고"),
-		WOMENS_UNIVERSITY("여대"),
-		;
-		
-		final String type;
-		SchoolType(String type){
-			this.type = type;
-		}
-		
-		public String getType() {
-			return type;
-		}
-		
 	}
 
 }
